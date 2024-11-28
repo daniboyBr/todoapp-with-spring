897016e3fc589bcd6ef1c1badf8697203e4ec9a4
Daniel Evangelista Pereira <daniel.evangelista93@gmail.com>
2024-11-27 21:58:35 -0300

    feat: add path to delete task on TaskController

--- src/main/java/com/app/todoapp/controller/TaskController.java
+++ src/main/java/com/app/todoapp/controller/TaskController.java
@@ -2,17 +2,18 @@ package com.app.todoapp.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.app.todoapp.models.Task;
 import com.app.todoapp.services.TaskService;
-import org.springframework.web.bind.annotation.PostMapping;
-
-
 
 @Controller
 @RequestMapping("/tasks")
@@ -37,6 +38,13 @@ public class TaskController {
         taskService.createTask(title);
         return "redirect:/tasks";
     }
+
+    @DeleteMapping("/{id}/delete")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        taskService.deleteTask(id);
+        return ResponseEntity.noContent().build();
+    }
+    
     
 
 }
